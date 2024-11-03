49b799e38885d3e658c579446a3b632ef85c7f51
danialchoudary <138550232+danialchoudary@users.noreply.github.com>
2024-11-03 15:29:39 +0500

    Update game.js

--- game.js
+++ game.js
@@ -1,73 +1,3 @@
-<<<<<<< HEAD:game.js
-let boxes=document.querySelectorAll(".box");
-let reset=document.querySelector("#reset");
-let newgame=document.querySelector("#newgame");
-let msg=document.querySelector(".msg");
-let turnX=true;
-let winpatterns=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
-];
-const Reset=()=>{
-    turnX=true;
-    enable();
-    msg.classList.add("hide");
-}
-
-
-boxes.forEach((box) => {
-    box.addEventListener("click",()=>{
-        if (turnX) {
-           box.innerHTML="O";
-           turnX=false; 
-        } else {
-            box.innerHTML="X";
-            turnX=true;
-        }
-        box.disabled=true;
-        checkwinner();
-    })
-});
-const disable=()=>{
-    for (const box of boxes) {
-        box.disabled=true;
-    }
-}
-const enable=()=>{
-    for (const box of boxes) {
-        box.disabled=false;
-        box.innerText="";
-    }
-}
-const showwinner=(winner)=>{
-    msg.innerText=`Winnner is ${winner}`;
-    msg.classList.remove("hide");
-    disable();
-}
-
-function checkwinner() {
-    for (let paterns of winpatterns) {
-    let posval1=boxes[paterns[0]].innerText;
-    let posval2=boxes[paterns[1]].innerText;
-    let posval3=boxes[paterns[2]].innerText;
-    if (posval1!=""&& posval2!=""&& posval3!="") {
-        if(posval1=== posval2 && posval2=== posval3){
-showwinner(posval1);
-    } 
-    }
-   
-    }
-       
-}
-    reset.addEventListener("click",Reset);
-    newgame.addEventListener("click",Reset);
-=======
 const boxes = document.querySelectorAll(".box");
 const resetButton = document.querySelector("#reset");
 const newGameButton = document.querySelector("#newgame");
@@ -83,7 +13,7 @@ let turnX = true;
 const winPatterns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
-    [0, 4, 8], [2, 4, 6]              // Diagonals
+    [0, 4, 8], [2, 4, 6]             
 ];
 
 const resetGame = () => {
@@ -156,4 +86,3 @@ newGameButton.addEventListener("click", () => {
     oWinsDisplay.textContent = oWins;
     resetGame();
 });
->>>>>>> 745fb2d (Modifies tic tac toe game):Tic tac toe/game.js
